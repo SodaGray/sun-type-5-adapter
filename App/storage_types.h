8c60dc3cc0de78350023463d7c72a0bae3830802
苏打灰 <prismarine@epicraft.top>
2026-06-11 16:49:54 +0800

    feat: add keyboard registry and storage types header

---
+++ App/storage_types.h
@@ -0,0 +1,14 @@
+//
+// Created by Cherry on 2026/6/11.
+//
+
+#ifndef STORAGE_TYPES_H
+#define STORAGE_TYPES_H
+
+typedef enum {
+    STORAGE_TYPE_REGISTRY = 1,   // device settings registry (instance 0)
+
+} storage_type_t;
+
+#endif //STORAGE_TYPES_H
+
